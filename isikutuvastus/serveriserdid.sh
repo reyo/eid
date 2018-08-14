8725312380224f719337828fa1a01d3db0d2b1cd
reyo <reijo.lell@gmail.com>
2018-08-14 17:35:39 +0300

    Uuendatud CA

--- isikutuvastus/serveriserdid.sh
+++ isikutuvastus/serveriserdid.sh
@@ -4,30 +4,31 @@
 
 openssl genrsa -out server.key 1024
 
-# Loo edaspidiseks signeerimiseks mõeldud fail server.csr, vastates mitmetele küsimustele:
+# Loo edaspidiseks signeerimiseks mÃµeldud fail server.csr, vastates mitmetele kÃ¼simustele:
 
 openssl req -new -key server.key -out server.csr
 
 # Loo katsetamiseks ise signeeritud fail server.crt. 
-# Seda ei pea veebiserver usaldusväärseks sertifikaadiks ning nõuab kasutajalt erikinnitust, 
-# et ta on ikka nõus selle sertifikaadiga saidile minema.
-# Taoline kinnituse küsimine ei ole muidugi aktsepteeritav avalikult kasutuses veebilehel, 
-# küll on ta aga OK katsetamiseks:
+# Seda ei pea veebiserver usaldusvÃ¤Ã¤rseks sertifikaadiks ning nÃµuab kasutajalt erikinnitust, 
+# et ta on ikka nÃµus selle sertifikaadiga saidile minema.
+# Taoline kinnituse kÃ¼simine ei ole muidugi aktsepteeritav avalikult kasutuses veebilehel, 
+# kÃ¼ll on ta aga OK katsetamiseks:
 
 openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
 
-# Ametliku ja usaldusväärse server.crt faili saad ise signeerimise asemel AS Sertifitseerimiskeskusest
+# Ametliku ja usaldusvÃ¤Ã¤rse server.crt faili saad ise signeerimise asemel AS Sertifitseerimiskeskusest
 # tellides: mine lehele http://sk.ee/teenused/veebiserveri-sertifikaadid . 
 # Selliselt tellitud sertifikaadifaili korral ei anna brauser enam mingeid hoiatusi.
 
 # Hangi AS Sertifitseerimiskeskusest kesksed sertifikaadid, vt ka https://sk.ee/repositoorium/sk-sertifikaadid/
 
-wget http://sk.ee/upload/files/JUUR-SK.PEM.cer
-wget http://sk.ee/upload/files/EECCRCA.pem.cer
-wget http://sk.ee/upload/files/ESTEID-SK%202007.PEM.cer
-wget http://sk.ee/upload/files/ESTEID-SK%202011.pem.cer
+wget https://sk.ee/upload/files/EE_Certification_Centre_Root_CA.pem.crt
+wget https://sk.ee/upload/files/ESTEID-SK_2011.pem.crt
+wget https://sk.ee/upload/files/ESTEID-SK_2015.pem.crt
+wget https://sk.ee/upload/files/EID-SK_2011.pem.crt
+wget https://sk.ee/upload/files/EID-SK_2016.pem.crt
 
-# Liida mitu sertifikaati üheks failiks:
+# Liida mitu sertifikaati Ã¼heks failiks:
 
-cat JUUR-SK.PEM.cer EECCRCA.pem.cer 'ESTEID-SK 2007.PEM.cer' 'ESTEID-SK 2011.pem.cer' > id.crt
+cat EE_Certification_Centre_Root_CA.pem.crt ESTEID-SK_2011.pem.crt ESTEID-SK_2015.pem.crt EID-SK_2011.pem.crt EID-SK_2016.pem.crt > id.crt
 
